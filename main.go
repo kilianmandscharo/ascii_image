69262cd63d704dbe8bbed31c20db4dec3e13e472
Dominik Heller <kilianmandscharo@gmail.com>
2024-08-11 17:01:46 +0200

    improve arg parsing

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"cmp"
+	"errors"
+	"flag"
+	"fmt"
 	"image"
 	"image/color"
 	"image/jpeg"
@@ -9,6 +13,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"golang.org/x/image/font"
@@ -20,32 +25,95 @@ const chunkSize = 10
 const numberOfCharacters = 9
 const fontHeight = 13
 
-var fontColor = color.RGBA{255, 207, 117, 255}
+var defaultFontColor = "255, 207, 117, 255"
 var characters = [numberOfCharacters]rune{'.', ':', 'c', 'o', 'P', 'O', '@', '■'}
 var allowedOuputFormats = []string{".jpg", ".jpeg", ".png"}
 
 func main() {
-	inputPath, outputPath := getArgs()
-	img := readImage(inputPath)
-	outImg := convertToAscii(img)
-	writeImage(outImg, outputPath)
+	options := getArgs()
+	img := readImage(options.inputPath)
+	outImg := convertToAscii(img, options.color)
+	writeImage(outImg, options.outputPath)
 }
 
-func getArgs() (string, string) {
-	if len(os.Args) < 2 {
-		log.Fatalf("Usage: %s <image input path> <image output path>", os.Args[0])
+type options struct {
+	inputPath  string
+	outputPath string
+	color      color.Color
+}
+
+func getArgs() *options {
+	var inputPathArg = flag.String("f", "", "the image input path")
+	var outputPathArg = flag.String("o", "", "the image output path")
+	var colorArg = flag.String("c", defaultFontColor, "the ascii font color in RGB / RGBA format")
+
+	flag.Parse()
+
+	if len(*inputPathArg) == 0 || len(*outputPathArg) == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
 
-	inputPath := os.Args[1]
+	outputFileExtension := filepath.Ext(*outputPathArg)
+	if !isAllowedOuputFormat(outputFileExtension) {
+		log.Fatalf(
+			"format '%s' is not an allowed output format, allowed formats: %s",
+			outputFileExtension,
+			strings.Join(allowedOuputFormats, " | "),
+		)
+	}
 
-	var outputPath string
-	if len(os.Args) >= 3 {
-		outputPath = os.Args[2]
+	color, err := parseRgbaColorString(cmp.Or(*colorArg, defaultFontColor))
+	if err != nil {
+		log.Fatalf("error parsing color string: %v", err)
+	}
+
+	return &options{
+		inputPath:  *inputPathArg,
+		outputPath: *outputPathArg,
+		color:      color,
+	}
+}
+
+func parseRgbaColorString(colorString string) (color.Color, error) {
+	color := color.RGBA{}
+	values := strings.Split(strings.Trim(colorString, "()"), ",")
+
+	if len(values) < 3 {
+		return color, errors.New("not enough color values")
+	}
+
+	r, err := strconv.Atoi(strings.TrimSpace(values[0]))
+	if err != nil {
+		return color, fmt.Errorf("invalid value '%s' for red: %v", values[0], err)
+	}
+
+	g, err := strconv.Atoi(strings.TrimSpace(values[1]))
+	if err != nil {
+		return color, fmt.Errorf("invalid value '%s' for green: %v", values[1], err)
+	}
+
+	b, err := strconv.Atoi(strings.TrimSpace(values[2]))
+	if err != nil {
+		return color, fmt.Errorf("invalid value '%s' for blue: %v", values[2], err)
+	}
+
+	var a int
+	if len(values) > 3 {
+		a, err = strconv.Atoi(strings.TrimSpace(values[3]))
+		if err != nil {
+			return color, fmt.Errorf("invalid value '%s' for alpha: %v", values[3], err)
+		}
 	} else {
-		outputPath = "output.jpg"
+		a = 255
 	}
 
-	return inputPath, outputPath
+	color.R = uint8(r)
+	color.G = uint8(g)
+	color.B = uint8(b)
+	color.A = uint8(a)
+
+	return color, nil
 }
 
 func readImage(inputPath string) image.Image {
@@ -93,7 +161,7 @@ func writeImage(img image.Image, outputPath string) {
 	log.Println("Image saved to:", outputPath)
 }
 
-func convertToAscii(img image.Image) image.Image {
+func convertToAscii(img image.Image, color color.Color) image.Image {
 	numberOfRowChunks := int(math.Ceil(float64(img.Bounds().Max.Y) / float64(chunkSize)))
 	numberOfColChunks := int(math.Ceil(float64(img.Bounds().Max.X) / float64(chunkSize)))
 
@@ -101,7 +169,7 @@ func convertToAscii(img image.Image) image.Image {
 
 	d := &font.Drawer{
 		Dst:  outImg,
-		Src:  image.NewUniform(fontColor),
+		Src:  image.NewUniform(color),
 		Face: basicfont.Face7x13,
 	}
 
